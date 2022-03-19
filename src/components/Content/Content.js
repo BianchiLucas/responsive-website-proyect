bd35b6dc96a1ef4a73deac7fd3c9e798d10678c3
BianchiLucas <lucasbianchi92@gmail.com>
2022-03-18 21:34:13 -0300

    Content and ContentStyles

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,4 +1,4 @@
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 import { Container, Section } from "../../globalStyles";
 import { ContentRow, TextWrapper, TopLine, Heading, ContentButton, Subtitle, ImgWrapper, Img, ContentColumn } from './ContentStyles'
 //Animación
@@ -7,27 +7,43 @@ import { useAnimation } from "framer-motion";
 
 
 const Content = () => {
+
+    //Animación
+    const initial = { opacity: 0, y: 30 }
+    const animation = useAnimation
+
+    const { ref, inView } = useInView({ threshold: 0.2 })
+
+    useEffect(() => {
+        if (inView) {
+            animation.start({
+                opacity: 1,
+                y: 0
+            })
+        }
+    }, [inView, animation])
+
   return (
-      <Section>
-          <Container>
-              <ContentRow>
-                  <ContentColumn>
-                      <TextWrapper>
-                          <TopLine></TopLine>
-                          <Heading></Heading>
-                          <Subtitle></Subtitle>
-                          <ContentButton></ContentButton>
-                      </TextWrapper>
-                  </ContentColumn>
-                  <ContentColumn>
-                      <ImgWrapper>
-                      < Img />
-                      </ImgWrapper>
-                  </ContentColumn>
-              </ContentRow>
-          </Container>
-      </Section>
-  )
+        <Section inverse={inverse} ref={ref} >
+            <Container>
+                <ContentRow>
+                    <ContentColumn>
+                        <TextWrapper>
+                            <TopLine initial={initial} transition={{ delay: 0.3, duration: 0.6 }} animate={animation} > {TopLine.text} </TopLine>
+                            <Heading initial={initial} transition={{ delay: 0.5, duration: 0.6 }} animate={animation} > {headine} </Heading>
+                            <Subtitle initial={initial} transition={{ delay: 0.7, duration: 0.6 }} animate={animation} > {description} </Subtitle>
+                            <ContentButton initial={initial} transition={{ delay: 1, duration: 0.6 }} animate={animation} inverse={inverse} primary={primary} > {buttonLabel} </ContentButton>
+                        </TextWrapper>
+                    </ContentColumn>
+                    <ContentColumn initial={initial} transition={{ delay: 0.5, duration: 0.6 }} animate={animation} >
+                        <ImgWrapper>
+                            < Img src={img} alt={alt} whileHover={{ rotate: 2, scale: 1.02 }} transition={{ duration: 0.5}} />
+                        </ImgWrapper>
+                    </ContentColumn>
+                </ContentRow>
+            </Container>
+        </Section>
+    )
 }
 
 export default Content
